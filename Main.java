38560c31ef874b552342852ce67f7ebc5adf0092
natsumikan07 <m.natsumi07@gmail.com>
2021-01-12 13:04:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+
+  public static void main(String[] args) {
+  class Local {
+    public void sayHello() {
+      System.out.println("Hello!");
+    }
+
+  }
+
+  Local local = new Local();
+
+  local.sayHello();
+
+  NoName noName = new NoName();
+
+  noName.runrun();
+
+  }
+}
